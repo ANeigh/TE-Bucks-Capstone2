478babd02fd8056d9e4e265a21b52ebe0375774d
Jared Hance <jarhance@gmail.com>
2023-03-13 10:05:51 -0400

    final capstone 2 :')

--- src/main/java/com/techelevator/tebucks/controller/UserController.java
+++ src/main/java/com/techelevator/tebucks/controller/UserController.java
@@ -4,11 +4,9 @@ import com.techelevator.tebucks.dao.JdbcTransferDao;
 import com.techelevator.tebucks.dao.TransferDao;
 import com.techelevator.tebucks.dao.UserDao;
 import com.techelevator.tebucks.model.Transfer;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.techelevator.tebucks.model.User;
 
 import java.security.Principal;
@@ -34,7 +32,7 @@ public UserController(UserDao userDao, TransferDao transferDao) {
     users.removeIf(x -> x.getId() == userDao.findIdByUsername(principal.getName()));
         return users;
     }
-
+@ResponseStatus(HttpStatus.OK)
 @RequestMapping(path = "/api/account/transfers", method = RequestMethod.GET)
     public List<Transfer> getAllTransfers(Principal principal) {
         return transferDao.getListOfTransfers(userDao.findByUsername(principal.getName()).getId());
